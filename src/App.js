bd684f18ea6e92cc77733dcb6c19e2533e5457ca
Mominur Bashar Ovi <mominur.bashar@numberskills.se>
2024-02-05 23:07:21 +0600

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Todo from "./todo";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { show: false };
+  }
+
+  render() {
+    // use Todo component inside render method.
+    return (
+      <div className="App">
+        {" "}
+        <Todo />{" "}
+      </div>
+    );
+  }
 }
 
 export default App;
