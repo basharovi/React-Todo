898652854f294b97074df6d107597ddc1f3285eb
Mominur Bashar Ovi <mominur.bashar@numberskills.se>
2024-02-05 23:15:46 +0600

    updated todo.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+
 import Todo from "./todo";
 
 class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { show: false };
+    this.state = {
+      show: false,
+    };
   }
 
   render() {
-    // use Todo component inside render method.
     return (
       <div className="App">
-        {" "}
-        <Todo />{" "}
+        <Todo />
       </div>
     );
   }
